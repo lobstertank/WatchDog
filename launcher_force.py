2de9702e6994db3ee47a63e55be2593ac4d4c3ef
everstudent <98907721+lobstertank@users.noreply.github.com>
2025-09-22 10:13:17 +0300

    Добавлен launcher_force.py для принудительного запуска основного бота независимо от выходных дней

---
+++ launcher_force.py
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Launcher для основного бота мониторинга Финолога с принудительной отправкой
+"""
+
+from contacts import MAIN_BOT_CONFIG
+from telegram_bot import main
+
+if __name__ == "__main__":
+    main(MAIN_BOT_CONFIG['bot_token'], MAIN_BOT_CONFIG['allowed_users'], is_test=False, force_check=True)
